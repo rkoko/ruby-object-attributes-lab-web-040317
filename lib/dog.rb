5770e748fe8571acdcc30be22d799d0bf19b1b94
rkoko <rinda.ko@gmail.com>
2017-04-05 14:10:08 -0400

    complete lab

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  #getter - return info on instance variable
+    def name
+      @name="Fido"
+    end
+  #setter  - modify existing insance variable
+    def name=(new_name)
+      @name=new_name
+    end
+#getter
+    def breed
+      @breed="Beagle"
+    end
+#setter
+    def breed=(new_breed)
+      @breed = new_breed
+    end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  #setter  - modify existing insance variable
+  def name=(new_name)
+    @name=new_name
+  end
+  #getter - return instance variable
+  def name
+    @name="Beyonce"
+  end
+
+  #setter  - modify existing insance variable
+  def job=(new_job)
+    @job = new_job
+  end
+  #getter - return instance variable
+  def job
+    @job="Singer"
+  end
+end
