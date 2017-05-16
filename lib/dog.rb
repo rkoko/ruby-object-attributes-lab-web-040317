0ea226a66cbbadc51b7d3de6ef3efca5b3d2fce1
rkoko <rinda.ko@gmail.com>
2017-05-15 23:16:14 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,18 @@
 class Dog
-  #getter - return info on instance variable
-    def name
-      @name="Fido"
-    end
-  #setter  - modify existing insance variable
-    def name=(new_name)
-      @name=new_name
-    end
-#getter
-    def breed
-      @breed="Beagle"
-    end
-#setter
-    def breed=(new_breed)
-      @breed = new_breed
-    end
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,19 +1,19 @@
 class Person
   #setter  - modify existing insance variable
-  def name=(new_name)
-    @name=new_name
+  def name= (name)
+    @name = name
   end
   #getter - return instance variable
-  def name
-    @name="Beyonce"
-  end
+def name
+  @name
+end
 
   #setter  - modify existing insance variable
-  def job=(new_job)
-    @job = new_job
-  end
+def job=(job)
+  @job = job
+end
   #getter - return instance variable
   def job
-    @job="Singer"
+    @job
   end
 end
